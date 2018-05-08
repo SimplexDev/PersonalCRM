67fcfb44f64fc30ffec7423324b00f21815bc2bc
Evan Hagberg <appdevevan@gmail.com>
2018-05-08 16:37:48 -0400

    Create backend for Contacts Index
    - Sanitized params for a signed in user
    - Prepare routes for DOM
    - Configure devise to signin/out
    - Configure routes.rb to set stage for index
    - Update seeds.rb with additional data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 User.destroy_all
 Contact.destroy_all
 Note.destroy_all
@@ -19,6 +13,8 @@ contacts = [
   {first_name: "Tom", last_name: "Dubcheck", company: "Costco", industry: "Sales", phone_number: "937-333-1212", how_met: "Met Tom at a Mark and Sally's wedding", user: user_1 },
   {first_name: "Derek", last_name: "Dubois", company: "Launch Lunch", industry: "IT", phone_number: "937-313- 4455", how_met: "Met Derek sailing the high seas of bermuda", user: user_2 },
   {first_name: "Jeffrey", last_name: "Unicorn", company: "Apple", industry: "Waste Management", phone_number: "666-333-1212", how_met: "Jeffrey and I met via Nick Alberts at a bonfire", user: user_3 },
+  {first_name: "Matt", last_name: "Macks", company: "Swift", industry: "Utilities", phone_number: "666-333-1212", how_met: "Matt is thoroughly interested in developing his Swift Contacts", user: user_1 },
+  {first_name: "Dugan", last_name: "Boo", company: "North Sails", industry: "Goodreads", phone_number: "666-333-1212", how_met: "Dugan is a noted soccer player, father of none, and enjoys disc golf", user: user_1 },
 ]
 
 contacts.each do |item|
@@ -30,7 +26,9 @@ contact_2 = Contact.last
 
 notes = [
   { comment: "I really enjoyed my meal with Sally, she had the duck confit but didn't like the sushi nibblins.  LOVES cosmopolitans and karaoke", user: user_1, contact: contact_1},
-  { comment: "Tom and Donna live in alaska, have two dogs Charlie and Max, love the avengers but didn't particularly care for thanos' skin color", user: user_2, contact: contact_2}
+  { comment: "Tom and Donna live in alaska, have two dogs Charlie and Max, love the avengers but didn't particularly care for thanos' attitude", user: user_2, contact: contact_2},
+  { comment: "Jefferey was a good binicorn, unfortunately he didn't last long", user: user_1, contact: contact_2},
+  { comment: "Samcake Sally; suddenly Susan", user: user_1, contact: contact_1}
 ]
 
 notes.each do |item|

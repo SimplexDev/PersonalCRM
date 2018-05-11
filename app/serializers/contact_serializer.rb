1c14fa4c66d42ffee0025d80a1a4b34d8a265992
Evan Hagberg <appdevevan@gmail.com>
2018-05-11 15:54:54 -0400

    Debug Contact Serializers
    - Nuke wayward spelling on line 2
    - Insert comma that was asmiss

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,5 +1,5 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :first_name, :last_name :full_name, :email, :company, :past_company, :industry, :phone_number, :how_met, :last_contacted, :follow_up, :locaiton, :family_details, :birthday
+  attributes :id, :user_id, :first_name, :last_name, :full_name, :email, :company, :past_company, :industry, :phone_number, :how_met, :last_contacted, :follow_up, :location, :family_details, :birthday
 
   belongs_to :user
 end

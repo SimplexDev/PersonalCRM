27f629646a3085a9781bcf04d352dc64270196f0
Evan Hagberg <appdevevan@gmail.com>
2018-05-14 17:00:53 -0400

    Redirect on login/out action with devise
    - Set Devise default paths
    - Added script to auto-timeout flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :store_action
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit({ role: ['member','admin'] }, :email, :first_name, :last_name, :full_name, :github_link, :linkedin_link, :password, :password_confirmation, :avatar)
+    end
+  end
+
+  def store_action
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      store_location_for(:user, "/contacts")
     end
   end
 end
